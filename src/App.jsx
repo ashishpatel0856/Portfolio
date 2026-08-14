1e139a70525efb8bae7d9f0a2cdd454f22ad3e9a
Ashish Patel <ashishkumarr0856@gmail.com>
2026-08-14 17:00:33 +0530

    update portfolio in coding profile

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Education from './components/Education'
-import CodingProfile from './components/CodingProfile'
 import Certifications from './components/Certifications'
 import Contact from './components/Contact'
 
